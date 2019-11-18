5012902683b0c0001671f92d4f78f37a5ebe9e78
Thiry Maxime <thiry96.maxime@gmail.com>
2019-11-18 10:54:52 +0100

    amélioration modale panier

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,6 @@ $(function() {
         'categorie': 'piece',
         'nom': 'Triens or du VIIème siècle',
         'prix': '1680',
-        'quantite': '1',
         'description': 'Mérovingens BAVDOMERE OR 1.26grs,Avers: + CABILO - NO FIT., Revers: + BAVDOMERES MONETARIVS. ,TTB+ ',
         'img': 'P01'
     };
@@ -12,7 +11,6 @@ $(function() {
         'categorie': 'piece',
         'nom': 'Double Henri d\'or 1er type',
         'prix': '5000',
-        'quantite': '1',
         'description': 'Monnaie, France, François II (1559-1560), Double Henri d\'or, 1560, Poitiers, TTB, Or Buste à cuirasse damasquinée de Henri II à droite Croix formée de 4 H couronnés, au centre la lettre d\'atelier, croissants en 1 et 3 et fleurs de lis en 2 et 4. HENRICVS.II.D.G.FRANC.REX Pièce de monnaie fabriquée au nom de Henri II',
         'img': 'P02'
     };
@@ -20,7 +18,6 @@ $(function() {
         'categorie': 'piece',
         'nom': '30 Sols au génie 1791',
         'prix': '112',
-        'quantite': '2',
         'description': 'argent 10grs',
         'img': 'P03'
     };
@@ -28,7 +25,6 @@ $(function() {
         'categorie': 'piece',
         'nom': '1 francs 1908',
         'prix': '6',
-        'quantite': '5',
         'description': 'Date de 1908',
         'img': 'P04'
     };
@@ -36,7 +32,6 @@ $(function() {
         'categorie': 'timbre',
         'nom': 'TIMBRE NAPOLEON N°18',
         'prix': '560',
-        'quantite': '1',
         'description': 'TIMBRE NAPOLEON N°18  - 1 Fr. neuf* réimpression 1862  signé Calvès   défaut avec clair.  voir photos  Côte 2200€',
         'img': 'T01'
     };
@@ -44,7 +39,6 @@ $(function() {
         'categorie': 'timbre',
         'nom': '10F orange Arc de Triomphe',
         'prix': '8',
-        'quantite': '4',
         'description': 'Neuf sans trace de charnière. Très beau.',
         'img': 'T02'
     };
@@ -52,7 +46,6 @@ $(function() {
         'categorie': 'timbre',
         'nom': 'Timbre poste 0,30',
         'prix': '0.31',
-        'quantite': '10',
         'description': 'TIMBRE POSTE N° 1509 OBLITERE',
         'img': 'T03'
     };
@@ -60,7 +53,6 @@ $(function() {
         'categorie': 'timbre',
         'nom': 'FRANCE Sage 35c',
         'prix': '200',
-        'quantite': '1',
         'description': 'FRANCE Sage n°93* 35c centrage courant frais signé Calves',
         'img': 'T04'
     };
@@ -68,7 +60,6 @@ $(function() {
         'categorie': 'oeuvre',
         'nom': 'Tableau thème marine signé Mabille',
         'prix': '100',
-        'quantite': '1',
         'description': 'Bon état général. Dimensions 61 x 38 cm',
         'img': 'O01'
     };
@@ -76,7 +67,6 @@ $(function() {
         'categorie': 'oeuvre',
         'nom': 'Les 3 roses',
         'prix': '25',
-        'quantite': '1',
         'description': 'Huile sur Toile de Bernard Le Béguec Format F3 27cm sur 22cm',
         'img': 'O02'
     };
@@ -84,7 +74,6 @@ $(function() {
         'categorie': 'oeuvre',
         'nom': 'Margueritte Deval – La devinette des Maturins',
         'prix': '10',
-        'quantite': '2',
         'description': ' Lithographie originale, signée  par l\'artiste, en haut à droite Tirée de la revue  "  LE  RIRE "   n° 274   du 3 fev.1900 , Feuille  seule  -  Publicités au dos .   Dimensions : 23 x 31 cm  Excellent état',
         'img': 'O03'
     };
@@ -92,7 +81,6 @@ $(function() {
         'categorie': 'oeuvre',
         'nom': 'Litho CHARENTON les CARRIERES',
         'prix': '15',
-        'quantite': '3',
         'description': 'RARE )  GUERRE  FRANCO  ALLEMANDE  1870 - 1871 SIEGE  DE  PARIS   1870         Militaria - CHARENTON LES CARRIERES  :  RETOUR A PARIS , 28 JANVIER 1871 . - Bérod, Editeur. (Paris) -- Date d´édition :…',
         'img': 'O04'
     };
@@ -100,7 +88,6 @@ $(function() {
         'categorie': 'meuble',
         'nom': 'Cabinet italien ',
         'prix': '3600',
-        'quantite': '1',
         'description': 'Epoque : 18ème siècle Etat : En l\'état  Matière : Placage Longueur : 78,5 Hauteur : 62 Profondeur : 35,5',
         'img': 'M01'
     };
@@ -108,7 +95,6 @@ $(function() {
         'categorie': 'meuble',
         'nom': 'Petit Argentier De Style Louis XVI ',
         'prix': '1150',
-        'quantite': '2',
         'description': 'poque : 19ème siècle Etat : Bon état  Matière : Noyer Largeur : 124 Hauteur : 210 Profondeur : 58',
         'img': 'M02'
     };
@@ -116,7 +102,6 @@ $(function() {
         'categorie': 'meuble',
         'nom': 'Cave à Liqueurs Fin 19ème ',
         'prix': '320',
-        'quantite': '5',
         'description': 'Epoque : 19ème siècle Etat : Très bon état  Matière : Cristal et carton Longueur : 27cm Hauteur : 23cm Profondeur : 21cm',
         'img': 'M03'
     };
@@ -124,7 +109,6 @@ $(function() {
         'categorie': 'meuble',
         'nom': 'Étagère Japonisante',
         'prix': '1850',
-        'quantite': '2',
         'description': 'De Gabriel Viardot D’époque Napoléon III, Artiste : Gabriel Viardot Epoque : 19ème siècle Etat : Bon état  Matière : Bois sculpté Largeur : 79 cm Hauteur : 75 cm Profondeur : 26 cm',
         'img': 'M04'
     };
@@ -132,7 +116,6 @@ $(function() {
         'categorie': 'voiture',
         'nom': 'Renault 4CV 1960',
         'prix': '19950',
-        'quantite': '1',
         'description': 'Points forts: -Magnifiquement restauré -Belle couleur bleu azur -Pare-chocs chromés -Tableau de bord blanc - Volant à deux branches - Sièges tissus gris/rouge',
         'img': 'V01'
     };
@@ -140,7 +123,6 @@ $(function() {
         'categorie': 'voiture',
         'nom': 'Triumph TR4A IRS 1966 ',
         'prix': '36950',
-        'quantite': '1',
         'description': ' Triumph TR4A IRS 1966 Overdrive  Points forts: - Belle couleur rouge - Jantes à rayons chromés - Overdrive - Feux antibrouillard - Capote Mohair - Sellerie luxe avec appuie-tête',
         'img': 'V02'
     };
@@ -148,7 +130,6 @@ $(function() {
         'categorie': 'voiture',
         'nom': 'Austin Healey Sprite MK1 1960',
         'prix': '24950',
-        'quantite': '1',
         'description': 'Points forts: - Version "Frogeye" MK1 - Moteur 1275cc avec carburateur Weber - Très bon état - Volant en bois Moto-Lita - Belle couleur noire',
         'img': 'V03'
     };
@@ -156,7 +137,6 @@ $(function() {
         'categorie': 'voiture',
         'nom': 'Mercedes-Benz 190SL 1956',
         'prix': '99000',
-        'quantite': '1',
         'description': 'Points forts • Moteur restauré • Bien entretenue • Peinture rouge',
         'img': 'V04'
     };
@@ -265,20 +245,23 @@ $(function() {
                                 if (arrayBag[i] == arrayContent[x][y]['img']) {
                                     $('#modalBag .modal-body').append(`
                                       <div class="${arrayContent[x][y]['img']} d-flex justify-content-between align-items-center mt-2">
-                                        <div>
+                                        <div class="d-flex">
                                           <img class="img-panier img-fluid" src="assets/img/${arrayContent[x][y]["img"]}.jpg" alt="">
-                                          <span class="ml-2">${arrayContent[x][y]['nom']}</span>
+                                          <div class="d-flex flex-column ml-3">
+                                            <span class="font-weight-bold">${arrayContent[x][y]['nom']}</span>
+                                            <span>Prix unitaire : ${arrayContent[x][y]['prix']}€</span>
+                                          </div>
                                         </div>
                                         <div class="d-flex align-items-center">
                                           <span>${arrayBag[i+1]}</span>
                                           <button type="button" class="btn-panier ml-2 addShoppingBagOne">
-                                             +
+                                            <i class="fas fa-plus"></i>
                                           </button>
                                           <button type="button" class="btn-panier ml-2 removeShoppingBagOne">
-                                             -
+                                            <i class="fas fa-minus"></i>
                                           </button>
                                           <button type="button" class="btn-panier ml-2 removeShoppingBag">
-                                             X
+                                            <i class="fas fa-trash-alt"></i>
                                           </button>
                                         </div>
                                       </div>
@@ -333,6 +316,7 @@ $(function() {
                       `)
                 }
                 // calcule le nouveau prix total du panier
+                console.log(arrayBag);
                 calcTotalPrice();
             }
         })
@@ -368,6 +352,7 @@ $(function() {
                 $(`.${ref}>div:nth-child(2)>span`).html(`${arrayBag[i+1]}`);
             }
         })
+        console.log(arrayBag);
         calcTotalPrice();
     }
 
@@ -384,6 +369,7 @@ $(function() {
                 $(`.${ref}>div:nth-child(2)>span`).html(`${arrayBag[i+1]}`);
             }
         })
+        console.log(arrayBag);
         calcTotalPrice();
     }
 
